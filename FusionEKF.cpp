add32be7e3be613693460468c4dad2c0786dc7ba
dirkmd <dirkmueller78@web.de>
2017-09-25 19:03:56 +0200

    Improvements after Project Review

--- FusionEKF.cpp
+++ FusionEKF.cpp
@@ -85,8 +85,12 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
-      ekf_.x_[0] = measurement_pack.raw_measurements_(0)*cos(measurement_pack.raw_measurements_(1));
-    	ekf_.x_[1] = measurement_pack.raw_measurements_(0)*sin(measurement_pack.raw_measurements_(1));
+      ekf_.x_[0] = measurement_pack.raw_measurements_(0)*cos(measurement_pack.raw_measurements_(1)); // px
+    	ekf_.x_[1] = measurement_pack.raw_measurements_(0)*sin(measurement_pack.raw_measurements_(1)); // py
+      ekf_.x_[2] = measurement_pack.raw_measurements_(2)*cos(measurement_pack.raw_measurements_(1)); // vx
+      ekf_.x_[3] = measurement_pack.raw_measurements_(2)*sin(measurement_pack.raw_measurements_(1)); // vy
+
+
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
@@ -126,49 +130,56 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
 	previous_timestamp_ = measurement_pack.timestamp_;
 
-	float dt_2 = dt * dt;
-	float dt_3 = dt_2 * dt;
-	float dt_4 = dt_3 * dt;
+	if ( dt > 0.001 ) {     //normal filter operation
+    float dt_2 = dt * dt;
+    float dt_3 = dt_2 * dt;
+    float dt_4 = dt_3 * dt;
 
-	//Modify the F matrix so that the time dt is integrated
-	ekf_.F_(0, 2) = dt;
-	ekf_.F_(1, 3) = dt;
+    //Modify the F matrix so that the time dt is integrated
+    ekf_.F_(0, 2) = dt;
+    ekf_.F_(1, 3) = dt;
 
-	//Modify the process covariance matrix Q with new dt
-	ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
-			   0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
-			   dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
-			   0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
+    //Modify the process covariance matrix Q with new dt
+    ekf_.Q_ <<  dt_4/4*noise_ax, 0, dt_3/2*noise_ax, 0,
+           0, dt_4/4*noise_ay, 0, dt_3/2*noise_ay,
+           dt_3/2*noise_ax, 0, dt_2*noise_ax, 0,
+           0, dt_3/2*noise_ay, 0, dt_2*noise_ay;
 
-	//predict
-  ekf_.Predict();
+    //predict
+    ekf_.Predict();
 
-  /*****************************************************************************
-   *  Update
-   ****************************************************************************/
+    /*****************************************************************************
+     *  Update
+     ****************************************************************************/
 
-  /**
-   TODO:
-     * Use the sensor type to perform the update step.
-     * Update the state and covariance matrices.
-   */
+    /**
+    TODO:
+      * Use the sensor type to perform the update step.
+      * Update the state and covariance matrices.
+    */
 
-  if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-    // Radar updates
-    // set Measurement Matrix H_ to the Jacobian Hj_
-    ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
-    // set Measurement Covariance Matrix to the given Matrix for Radar
-    ekf_.R_ = R_radar_;
-    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
-  } 
-  else {
-    // Laser updates
-    // set Measurement Matrix H_ to the initialized Matrix H_laser_
-    ekf_.H_ = H_laser_;
-    // set Measurement Covariance Matrix to the given Matrix for Radar
-    ekf_.R_ = R_laser_;
-    ekf_.Update(measurement_pack.raw_measurements_);
+    if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
+      // Radar updates
+      // set Measurement Matrix H_ to the Jacobian Hj_
+      ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+      // set Measurement Covariance Matrix to the given Matrix for Radar
+      ekf_.R_ = R_radar_;
+      ekf_.UpdateEKF(measurement_pack.raw_measurements_);
+    } 
+    else {
+      // Laser updates
+      // set Measurement Matrix H_ to the initialized Matrix H_laser_
+      ekf_.H_ = H_laser_;
+      // set Measurement Covariance Matrix to the given Matrix for Radar
+      ekf_.R_ = R_laser_;
+      ekf_.Update(measurement_pack.raw_measurements_);
+    }
+
   }
+  else { // do nothing because of arithmetic inconsistency
+
+  }
+  
 
   // print the output
   cout << "x_ = " << ekf_.x_ << endl;
